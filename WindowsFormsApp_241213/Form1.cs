06c13d7af73daf3c493df3fe6be57429f8ae87a4
rninji24 <pmj_1124@naver.com>
2024-12-17 13:51:49 +0900

    241217 practice2

--- WindowsFormsApp_241213/Form1.cs
+++ WindowsFormsApp_241213/Form1.cs
@@ -23,6 +23,10 @@ namespace WindowsFormsApp_241213
              */
             // textBox_print.Text = "이것은\r\n멀티라인\r\n텍스트박스\r\n인데요.";
 
+            // ctrl + k + c 블럭 전체 주석 처리
+            // ctrl + k + u 블럭 전체 주석 해제
+
+            #region 241213
             //241213
             //byte theNumberOfStudent = 30;
             //short fiveHourToSecond= 18000;
@@ -43,27 +47,54 @@ namespace WindowsFormsApp_241213
             //textBox_print.Text += " incomeTax: " + incomeTaxPercent.ToString() + "\r\n";
             //textBox_print.Text += grossDomesticProduct_korea.GetType();
             //textBox_print.Text += " grossDomesticProduct: " + grossDomesticProduct_korea.ToString() + "\r\n";
+            #endregion
 
-
-            // 241217
-            string[] stringArr = new string[10];
-            stringArr[0] = "동해 물과 백두산이".IndexOf("백두산").ToString();
-            stringArr[1] = "토요일에 먹는 토마토".LastIndexOf("토").ToString();
-            stringArr[2] = "질서 있는 퇴장".Contains("퇴").ToString();
-            stringArr[3] = "그 사람의 그림자는 그랬다".Replace("그", "이").ToString();
-            stringArr[4] = "삼성 갤럭시".Insert(2, " 애플");
-            stringArr[5] = "오늘은 왠지 더 배고프다".Remove(7, 1);
-            string[] tmp = "이름,나이,전화번호".Split(',');
-            stringArr[6] = tmp[0];
-            stringArr[7] = tmp[1];
-            stringArr[8] = tmp[2];
-            stringArr[9] = "우리 나라 만세".Substring(3, 2);
+            #region 241217 실습1
+            //string[] stringArr = new string[10];
+            //stringArr[0] = "동해 물과 백두산이".IndexOf("백두산").ToString();
+            //stringArr[1] = "토요일에 먹는 토마토".LastIndexOf("토").ToString();
+            //stringArr[2] = "질서 있는 퇴장".Contains("퇴").ToString();
+            //stringArr[3] = "그 사람의 그림자는 그랬다".Replace("그", "이").ToString();
+            //stringArr[4] = "삼성 갤럭시".Insert(2, " 애플");
+            //stringArr[5] = "오늘은 왠지 더 배고프다".Remove(7, 1);
+            //string[] tmp = "이름,나이,전화번호".Split(',');
+            //stringArr[6] = tmp[0];
+            //stringArr[7] = tmp[1];
+            //stringArr[8] = tmp[2];
+            //stringArr[9] = "우리 나라 만세".Substring(3, 2);
             
-            textBox_print.Text = "";
-            for(int i = 0; i<10; i++)
+            //textBox_print.Text = "";
+            //for(int i = 0; i<10; i++)
+            //{
+            //    textBox_print.Text += stringArr[i] + "\r\n";
+            //}
+            #endregion
+
+
+            // 241217 실습2
+            string message = "멈추지 않는 한 얼마나 천천히 가는지는 중요하지 않다.-공자";
+
+            // 1 "-공자" 부분 삭제
+            int findidx = message.LastIndexOf("-");
+            string delete_gongja = message.Remove(findidx, 3);
+            textBox_print.Text = delete_gongja + "\r\n";
+
+            // 2 "얼마나", "천천히", "가는지" 단어 각각 나눠서 배열에 저장
+            int keyword1_index = message.IndexOf("얼마나");
+            int keyword2_index = message.LastIndexOf("가는지");
+            string message2 = message.Substring(keyword1_index, (keyword2_index+3) - keyword1_index);
+            string[] split = message2.Split(' ');
+            for(int i = 0;i < split.Length; i++)
             {
-                textBox_print.Text += stringArr[i] + "\r\n";
+                textBox_print.Text += split[i] + "\r\n";
             }
+
+            // 3 ".", "-" 제거, 공백 문자(" ") ","로 바꾸기
+            string remove_dot = message.Remove(message.LastIndexOf('.'),1);
+            string remove = remove_dot.Remove(remove_dot.LastIndexOf('-'),1);
+            string replace = remove.Replace(' ', ',');
+            textBox_print.Text += replace + "\r\n";
+
         }
 
       
