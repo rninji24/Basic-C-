1aa84282bcc9470868ea0a06d21f7558b5148519
rninji24 <pmj_1124@naver.com>
2024-12-18 12:12:03 +0900

    241218 practice2

--- WindowsFormsApp_241213/Practice241218.cs
+++ WindowsFormsApp_241213/Practice241218.cs
@@ -15,17 +15,36 @@ namespace WindowsFormsApp_241213
         public Practice241218()
         {
             InitializeComponent();
-            int num = 30;
-            int num2 = 4;
-            int[] result = clac_division_mod(num, num2);
-            textBox_print.Text = result[0].ToString() + "\r\n";
-            textBox_print.Text += result[1].ToString() + "\r\n";
+
+            #region practice1
+            //int num = 30;
+            //int num2 = 4;
+            //int[] result = clac_divide_remain(num, num2);
+            //textBox_print.Text = result[0].ToString() + "\r\n";
+            //textBox_print.Text += result[1].ToString() + "\r\n";
+            #endregion
+
+            #region practice2
+            bool coin_fornt = true;
+            bool coin_back = false;
+            bool result = coinToss_winorlose(coin_fornt);
+            if (result == true)
+            {
+                textBox_print.Text = "승리";
+            }
+            else
+            {
+                textBox_print.Text += "패배";
+            }
+            #endregion
+
 
         }
 
-        int[] clac_division_mod(int x, int y)
+        #region practice1 함수
+        int[] clac_divide_remain(int x, int y)
         {
-        
+
             int result = x / y;
             int result2 = x % y;
             int[] ints = new int[2];
@@ -33,7 +52,24 @@ namespace WindowsFormsApp_241213
             ints[1] = result2;
             return ints;
         }
+        #endregion
+
+        #region practice2 함수
+        Boolean coinToss_winorlose(Boolean front_back)
+        {
+            Random random = new Random();
+            int a = random.Next(0, 2);
+            if (a == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
 
     }
-    
+
 }
