2d47706fe5f95a5e76e1d7398e9c40a427a15485
rninji24 <pmj_1124@naver.com>
2024-12-20 13:05:57 +0900

    241220 practice2

--- WindowsFormsApp_241213/Practice241220.Designer.cs
+++ WindowsFormsApp_241213/Practice241220.Designer.cs
@@ -29,47 +29,100 @@
         private void InitializeComponent()
         {
             this.textBox_result = new System.Windows.Forms.TextBox();
-            this.textBox_input = new System.Windows.Forms.TextBox();
-            this.button_input = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox_myChoice = new System.Windows.Forms.TextBox();
+            this.textBox_computerChoice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_result
             // 
             this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.textBox_result.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_result.Location = new System.Drawing.Point(91, 108);
+            this.textBox_result.Location = new System.Drawing.Point(91, 156);
             this.textBox_result.Multiline = true;
             this.textBox_result.Name = "textBox_result";
             this.textBox_result.ReadOnly = true;
             this.textBox_result.Size = new System.Drawing.Size(601, 282);
             this.textBox_result.TabIndex = 2;
             // 
-            // textBox_input
+            // button1
             // 
-            this.textBox_input.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_input.Location = new System.Drawing.Point(91, 64);
-            this.textBox_input.Name = "textBox_input";
-            this.textBox_input.Size = new System.Drawing.Size(500, 25);
-            this.textBox_input.TabIndex = 0;
+            this.button1.Location = new System.Drawing.Point(91, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "가위";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button_input
+            // button2
             // 
-            this.button_input.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button_input.Location = new System.Drawing.Point(597, 60);
-            this.button_input.Name = "button_input";
-            this.button_input.Size = new System.Drawing.Size(85, 35);
-            this.button_input.TabIndex = 1;
-            this.button_input.Text = "input";
-            this.button_input.UseVisualStyleBackColor = true;
-            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            this.button2.Location = new System.Drawing.Point(299, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "바위";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(504, 59);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "보";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox_myChoice
+            // 
+            this.textBox_myChoice.Location = new System.Drawing.Point(173, 119);
+            this.textBox_myChoice.Name = "textBox_myChoice";
+            this.textBox_myChoice.Size = new System.Drawing.Size(173, 25);
+            this.textBox_myChoice.TabIndex = 6;
+            // 
+            // textBox_computerChoice
+            // 
+            this.textBox_computerChoice.Location = new System.Drawing.Point(489, 119);
+            this.textBox_computerChoice.Name = "textBox_computerChoice";
+            this.textBox_computerChoice.Size = new System.Drawing.Size(173, 25);
+            this.textBox_computerChoice.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "사용자";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "컴퓨터";
             // 
             // Practice241220
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button_input);
-            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_computerChoice);
+            this.Controls.Add(this.textBox_myChoice);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox_result);
             this.Name = "Practice241220";
             this.Text = "Practice241220";
@@ -81,7 +134,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_result;
-        private System.Windows.Forms.TextBox textBox_input;
-        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox_myChoice;
+        private System.Windows.Forms.TextBox textBox_computerChoice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- WindowsFormsApp_241213/Practice241220.cs
+++ WindowsFormsApp_241213/Practice241220.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +13,10 @@ namespace WindowsFormsApp_241213
 {
     public partial class Practice241220 : Form
     {
+
         public Practice241220()
         {
-            InitializeComponent();
-            
+            InitializeComponent();           
         }
 
         void makeSentence(string name, int score)
@@ -23,19 +24,114 @@ namespace WindowsFormsApp_241213
             textBox_result.Text += name + ": " + score + "점\r\n";
         }
         private void button_input_Click(object sender, EventArgs e)
+        {        
+            #region practice1
+            //int student_num = int.Parse(textBox_input.Text);
+            //int[] student_score = new int[student_num];
+            //textBox_result.Text = "";
+            //for (int i = 0; i < student_num; i++)
+            //{
+            //    int score = random.Next(101);
+            //    student_score[i] = score;
+            //    string studentName = "학생" + (i+1).ToString();
+            //    makeSentence(studentName, student_score[i]);        
+            //}
+            #endregion
+           
+        }
+
+        int computerRSP()
         {
-            Random random = new Random();
-            
-            int student_num = int.Parse(textBox_input.Text);
-            int[] student_score = new int[student_num];
-            textBox_result.Text = "";
-            for (int i = 0; i < student_num; i++)
+            Random r = new Random();
+            int x = r.Next(0, 3);
+            if (x == 0)
             {
-                int score = random.Next(101);
-                student_score[i] = score;
-                string studentName = "학생" + (i+1).ToString();
-                makeSentence(studentName, student_score[i]);        
+                textBox_computerChoice.Text = "가위";
             }
+            else if (x == 1)
+            {
+                textBox_computerChoice.Text = "바위";
+            }
+            else if (x == 2)
+            {
+                textBox_computerChoice.Text = "보";
+            }
+            return x;
+        }
+
+        int use_win = 0;
+        int computer_win = 0;
+        string[] match = { "가위", "바위", "보" };
+
+        private void Match(int idx, int computer)
+        {
+            if (idx == 0 && computer == 2)
+            {
+                use_win++;
+            }
+            else if (idx == 1 && computer == 0)
+            {
+                use_win++;
+            }
+            else if (idx == 2 && computer == 1)
+            {
+                use_win++;
+            }
+            else if (idx == 2 && computer == 0)
+            {
+                computer_win++;
+            }
+            else if (idx == 0 && computer == 1)
+            {
+                computer_win++;
+            }
+            else if (idx == 1 && computer == 2)
+            {
+                computer_win++;
+            }
+            textBox_result.Text += "사용자: " + match[idx] + " 컴퓨터: " + match[computer] + " | 점수 사용자: " + use_win.ToString() + " 컴퓨터: " + computer_win.ToString() + "\r\n";
+
+            if (use_win == 3 || computer_win == 3)
+            {
+                if (use_win == 3 && computer != 3)
+                {
+                    textBox_result.Text += "사용자 승리\r\n";
+                }
+                else if (use_win != 3 || computer_win == 3)
+                {
+                    textBox_result.Text += "컴퓨터 승리\r\n";
+                }
+                else
+                {
+                    textBox_result.Text += "무승부\r\n";
+                }
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_myChoice.Text = "가위";
+            int idx = 0;
+            int computer = computerRSP();
+            Match(idx, computer);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox_myChoice.Text = "바위";
+            int idx = 1;
+            int computer = computerRSP();
+            Match(idx, computer);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox_myChoice.Text = "보";
+            int idx = 2;
+            int computer = computerRSP();
+            Match(idx, computer);
         }
     }
 }
